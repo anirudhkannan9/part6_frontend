f5219e2fb24b2309552b07db0a2950022e0b4451
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-17 16:59:38 -0700

    Ex 6.15: installed redux-thunk. Modified initialization of redux-store to happen using asynchronous action-creators; abstracted interaction w/ server away from components.

--- submissions/anecdotes-redux/src/store.js
+++ submissions/anecdotes-redux/src/store.js
@@ -1,8 +1,11 @@
+
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notifReducer from './reducers/notifReducer'
 import filterReducer from './reducers/filterReducer';
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createStore, combineReducers } from 'redux'
 
 const reducer = combineReducers({
     anecdotes: anecdoteReducer,
@@ -12,7 +15,9 @@ const reducer = combineReducers({
 
 const store = createStore(
     reducer,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
 )
 
 export default store
